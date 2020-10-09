cffc11fa419458758c80d48a7cf953a9c4268e03
artem <xaputo@gmail.com>
2020-10-09 15:58:16 +0300

    Add tests

---
+++ src/components/FullItem/FullItem.spec.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import FullItem from './FullItem.component';
+
+describe('<FullItem/>', () => {
+  it('should render properly', () => {
+    const wrapper = mount(
+      <FullItem
+        name="Test"
+        image="123"
+        price={25}
+        description="yo"
+      />,
+    );
+    expect(wrapper.find('Projects')).toHaveLength(0);
+  });
+});
